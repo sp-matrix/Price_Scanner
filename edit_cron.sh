ade0d0eb9f2716e7851c991111f7c57f2f8e7abf
sp-matrix <42920594+sp-matrix@users.noreply.github.com>
2023-08-07 13:15:48 -0400

    Update edit_cron.sh

--- edit_cron.sh
+++ edit_cron.sh
@@ -1,1 +1,20 @@
+#!/bin/ksh
 
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <keyword>"
+    exit 1
+fi
+
+keyword=$1
+
+if [[ $keyword == "ALL" ]]; then
+    crontab -l | sed 's/^/#/' | crontab -
+else
+    crontab -l | sed "/$keyword/s/^/#/" | crontab -
+fi
+### for uncommenting
+if [[ $keyword == "ALL" ]]; then
+    crontab -l | sed 's/^#//' | crontab -
+else
+    crontab -l | sed "/$keyword/s/^#//" | crontab -
+fi
