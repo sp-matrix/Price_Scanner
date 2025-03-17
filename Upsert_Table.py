a60335a2eb3cf20e8a8c5bbc237641b850235498
sp-matrix <42920594+sp-matrix@users.noreply.github.com>
2025-03-16 21:53:42 -0400

    Create Upsert_Table.py

---
+++ Upsert_Table.py
@@ -0,0 +1,17 @@
+# upsert_table.py
+dbutils.fs.put("/FileStore/write_to_table.py", """
+from pyspark.sql import SparkSession
+import sys
+
+if len(sys.argv) > 1:
+    input_value = sys.argv[1]
+else:
+    raise ValueError("No input value provided")
+
+spark = SparkSession.builder.appName("WriteToTable").getOrCreate()
+data = [(input_value,)]
+columns = ["value"]
+df = spark.createDataFrame(data, columns)
+df.write.mode("append").saveAsTable("your_table_name")
+spark.stop()
+""", True)
